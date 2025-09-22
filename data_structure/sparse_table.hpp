31d784668594568f3790afbfabcac60275eff284
tanaka255 <105200487+tanaka255@users.noreply.github.com>
2025-09-22 04:26:38 +0000

    feat: Sparse Tableを追加

---
+++ data_structure/sparse_table.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "template.hpp"
+
+template <class S, S (*op)(S, S), S (*e)()>
+struct sparse_table {
+  int n;
+  vector<vector<S>> st;
+
+  sparse_table() : n(0) {}
+  sparse_table(vector<S> v) : n(v.size()) {
+    if (n == 0) return;
+    int log_n = 0;
+    while ((1 << log_n) <= n) ++log_n;
+    st.assign(log_n, vector<S>(n));
+
+    st[0] = v;
+    rep(k, log_n - 1) {
+      for (int i = 0; i + (1 << (k + 1)) <= n; ++i) {
+        st[k + 1][i] = op(st[k][i], st[k][i + (1 << k)]);
+      }
+    }
+  }
+
+  S prod(int l, int r) {
+    if (l >= r) return e();
+    int k = 31 - __builtin_clz(r - l);
+    return op(st[k][l], st[k][r - (1 << k)]);
+  }
+};
