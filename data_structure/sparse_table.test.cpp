7d5067f4f06000e7c8188f650c8d8a31a6decf74
tanaka255 <105200487+tanaka255@users.noreply.github.com>
2025-09-22 08:08:57 +0000

    test: Sparse Tableのテストを追加

---
+++ data_structure/sparse_table.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+#include "data_structure/sparse_table.hpp"
+
+#include "template.hpp"
+int op(int a, int b) { return min(a, b); }
+int e() { return 1 << 30; }
+signed main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  int N, Q;
+  cin >> N >> Q;
+  vector<int> a(N);
+  rep(i, N) cin >> a[i];
+  sparse_table<int, op, e> st(a);
+  rep(_, Q) {
+    int l, r;
+    cin >> l >> r;
+    cout << st.prod(l, r) << '\n';
+  }
+}
