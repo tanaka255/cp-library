0771e86f9d6988c8f32e2ca79c65c80adb1b35fe
tanaka255 <105200487+tanaka255@users.noreply.github.com>
2025-09-22 07:59:35 +0000

    fix: includeを修正

--- trees/lowest_common_ancestor.test.cpp
+++ trees/lowest_common_ancestor.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM \
   "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_C"
-#include "graph/lowest_common_ancestor.hpp"
+#include "trees/lowest_common_ancestor.hpp"
 
 #include "template.hpp"
 signed main() {
