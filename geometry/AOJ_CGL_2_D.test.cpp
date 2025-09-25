fd597682dbff8d3fc4f63167afcd9ee544b56afb
tanaka255 <105200487+tanaka255@users.noreply.github.com>
2025-09-25 06:18:52 +0000

    test: 2線分間の距離を求めるライブラリのテストを追加

---
+++ geometry/AOJ_CGL_2_D.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_D"
+#define ERROR 0.00000001
+#include "geometry/core.hpp"
+#include "template.hpp"
+signed main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  cout << fixed << setprecision(20);
+  int q;
+  cin >> q;
+  rep(_, q) {
+    vector<Point> p(4);
+    rep(i, 4) {
+      double x, y;
+      cin >> x >> y;
+      p[i] = Point(x, y);
+    }
+    Line s1(p[0], p[1]), s2(p[2], p[3]);
+    cout << distance_between_segments(s1, s2) << '\n';
+  }
+}
