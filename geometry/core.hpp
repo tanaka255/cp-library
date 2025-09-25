cfab004d0a629333b2a7f75b861a796a8a3acc02
tanaka255 <105200487+tanaka255@users.noreply.github.com>
2025-09-25 05:18:00 +0000

    refactor: 冗長なコード等を修正

--- geometry/core.hpp
+++ geometry/core.hpp
@@ -21,25 +21,19 @@ Point projection(const Point& p0, const Point& p1, const Point& p) {
 }
 
 Point reflection(const Point& p0, const Point& p1, const Point& p) {
-  Point proj = projection(p0, p1, p);
-  return 2.0 * proj - p;
+  return 2.0 * projection(p0, p1, p) - p;
 }
 
-constexpr int COUNTER_CLOCKWISE = 1;
-constexpr int CLOCKWISE = -1;
-constexpr int ONLINE_BACK = 2;
-constexpr int ONLINE_FRONT = -2;
-constexpr int ON_SEGMENT = 0;
+constexpr int COUNTER_CLOCKWISE = 1, CLOCKWISE = -1, ONLINE_BACK = 2,
+              ONLINE_FRONT = -2, ON_SEGMENT = 0;
 
 int ccw(const Point& p0, const Point& p1, const Point& p2) {
   Point a = p1 - p0, b = p2 - p0;
   if (eps < cross(a, b)) return COUNTER_CLOCKWISE;
   if (cross(a, b) < -eps) return CLOCKWISE;
   if (dot(a, b) < -eps) return ONLINE_BACK;
-  if (norm(a) < norm(b))
-    return ONLINE_FRONT;
-  else
-    return ON_SEGMENT;
+  if (norm(a) < norm(b)) return ONLINE_FRONT;
+  return ON_SEGMENT;
 }
 
 bool is_parallel(const Line& l0, const Line& l1) {
@@ -51,20 +45,13 @@ bool is_orthogonal(const Line& l0, const Line& l1) {
 }
 
 bool has_intersection(Line s0, Line s1) {
-  rep(_, 2) {
-    if (0 < ccw(s0.a, s0.b, s1.a) * ccw(s0.a, s0.b, s1.b)) {
-      return false;
-    }
-    swap(s0, s1);
-  }
-  return true;
+  return ccw(s0.a, s0.b, s1.a) * ccw(s0.a, s0.b, s1.b) <= 0 &&
+         ccw(s1.a, s1.b, s0.a) * ccw(s1.a, s1.b, s0.b) <= 0;
 }
 
 Point cross_point(const Line& l0, const Line& l1) {
   double d0 = cross(l0.b - l0.a, l1.b - l1.a),
          d1 = cross(l0.b - l0.a, l0.b - l1.a);
-  if (abs(d0) < eps && abs(d1) < eps) {
-    return l1.a;
-  }
+  if (abs(d0) < eps && abs(d1) < eps) return l1.a;
   return l1.a + d1 / d0 * (l1.b - l1.a);
 }
