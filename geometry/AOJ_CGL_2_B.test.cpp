ea45eb936a22b41adcc450ed583781181ed24c61
tanaka255 <105200487+tanaka255@users.noreply.github.com>
2025-09-25 04:07:53 +0000

    test: 2線分の交点の有無を判定するライブラリのテストを追加

---
+++ geometry/AOJ_CGL_2_B.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/1/CGL_2_B"
+#include "geometry/core.hpp"
+#include "template.hpp"
+signed main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  int q;
+  cin >> q;
+  rep(_, q) {
+    vector<Point> p(4);
+    rep(i, 4) {
+      double x, y;
+      cin >> x >> y;
+      p[i] = Point(x, y);
+    }
+    Line s1(p[0], p[1]), s2(p[2], p[3]);
+    cout << has_intersection(s1, s2) << '\n';
+  }
+}
